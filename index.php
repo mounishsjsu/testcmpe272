0723564d6238217cee9e20aaf5411bb1f97e5e32
mounishsjsu <111546831+mounishsjsu@users.noreply.github.com>
2022-10-07 16:49:19 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $pagedata['index'] = array(
  'title'=>'Fitness Club',
 );
 $pagedata['about'] = array(
- 'title'=>'What is this page abput',
+ 'title'=>'What is this page about',
 );
 $pagedata['courses'] = array(
  'title'=>'Title for Images',
@@ -29,82 +29,6 @@ header('Content-type: text/html; charset=utf-8');
 <head>
  <meta charset="utf-8" />
  <title><?php echo $title; ?></title>
- <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
- <style><!--
- body {
- margin:0;
- text-align:center;
- padding:0 1em;
- }
- header, footer, section, aside, nav, article { display: block; }
- #posts{
- position:relative;
- width:99%;
- margin:0.5em auto;
- background:#fdfefe;
- } 
-.nav ul {
-  list-style: none;
-  background-color: #444;
-  text-align: center;
-  padding: 0;
-  margin: 0;
-}
-.nav li {
-  font-family: 'Oswald', sans-serif;
-  font-size: 1.2em;
-  line-height: 40px;
-  height: 40px;
-  border-bottom: 1px solid #888;
-}
- 
-.nav a {
-  text-decoration: none;
-  color: #fff;
-  display: block;
-  transition: .3s background-color;
-}
- 
-.nav a:hover {
-  background-color: #005f5f;
-}
- 
-.nav a.active {
-  background-color: #fff;
-  color: #444;
-  cursor: default;
-}
- 
-@media screen and (min-width: 600px) {
-  .nav li {
-    width: 120px;
-    border-bottom: none;
-    height: 50px;
-    line-height: 50px;
-    font-size: 1.4em;
-  }
-
-  .nav li {
-    display: inline-block;
-    margin-right: -4px;
-  }
- 
-}
- #article {
- margin:0 1em 0 16em;
- background:#efeffe;
- border:1px solid #01da02;
- padding:0.2em 0.4em;
- }
- #footer {
- clear:both;
- position:relative;
- background:#edfeed;
- border:1px solid #dada01;
- width:99%;
- margin:2em auto 0.5em auto;
- }
- --></style>
 </head>
 <body>
 
@@ -115,7 +39,7 @@ header('Content-type: text/html; charset=utf-8');
 
 <div class="nav">
  <ul>
- <li><a href="index.php" title="Home page">Home</a></li>
+ <li><a href="index.php?" title="Home page">Home</a></li>
  <li><a href="index.php?pg=about" title="About">About</a></li>
  <li><a href="index.php?pg=courses" title="Images">courses</a></li>
  <li><a href="index.php?pg=blogs" title="Images">blogs</a></li>
