69294ce374a675ddf51d558a39e31182e07ff907
mounishsjsu <111546831+mounishsjsu@users.noreply.github.com>
2022-10-07 16:26:05 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,31 +1,25 @@
 <?php
-  /* Create simple Website with PHP - http://coursesweb.net/php-mysql/ */
-
-// create an array with data for title, and meta, for each page
-$pgdata = array();
-$pgdata['index'] = array(
-  'title'=>'Title for Home page',
-  'description'=>'Here add the description for Home page',
-  'keywords'=>'meta keywords, for, home page'
+$pagegdata = array();
+$pagedata['index'] = array(
+ 'title'=>'Fitness Club',
 );
-$pgdata['about_me'] = array(
-  'title'=>'Title for About Me page',
-  'description'=>'Description for About Me, http://coursesweb.net',
-  'keywords'=>'about me, coursesweb.net'
+$pagedata['about'] = array(
+ 'title'=>'What is this page abput',
 );
-$pgdata['images'] = array(
-  'title'=>'Title for Images',
-  'description'=>'Here add the description for the page with images',
-  'keywords'=>'images, pictures, photo'
+$pagedata['courses'] = array(
+ 'title'=>'Title for Images',
+);
+$pagedata['blogs'] = array(
+    'title'=>'Title for Images',
+);
+$pagedata['contacts'] = array(
+    'title'=>'Title for Images',
 );
 
-// set the page name
-$pgname = isset($_GET['pg']) ? trim(strip_tags($_GET['pg'])) : 'index';
 
-// get title, and meta data for current /accessed page
-$title = $pgdata[$pgname]['title'];
-$description = $pgdata[$pgname]['description'];
-$keywords = $pgdata[$pgname]['keywords'];
+$pagename = isset($_GET['pg']) ? trim(strip_tags($_GET['pg'])) : 'index';
+
+$title = $pagedata[$pagename]['title'];
 
 // set header for utf-8 encode
 header('Content-type: text/html; charset=utf-8');
@@ -35,47 +29,80 @@ header('Content-type: text/html; charset=utf-8');
 <head>
  <meta charset="utf-8" />
  <title><?php echo $title; ?></title>
- <meta name="description" content="<?php echo $description; ?>" />
- <meta name="keywords" content="<?php echo $keywords; ?>" />
  <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
  <style><!--
  body {
-  margin:0;
-  text-align:center;
-  padding:0 1em;
+ margin:0;
+ text-align:center;
+ padding:0 1em;
  }
  header, footer, section, aside, nav, article { display: block; }
  #posts{
-  position:relative;
-  width:99%;
-  margin:0.5em auto;
-  background:#fdfefe;
- }
- #menu {
-  float:left;
-  width:15em;
-  margin:0 auto;
-  background:#f8f9fe;
-  border:1px solid blue;
-  text-align:left;
- }
- #menu li a:hover {
-   text-decoration:none;
-   color:#01da02;
- }
+ position:relative;
+ width:99%;
+ margin:0.5em auto;
+ background:#fdfefe;
+ } 
+.nav ul {
+  list-style: none;
+  background-color: #444;
+  text-align: center;
+  padding: 0;
+  margin: 0;
+}
+.nav li {
+  font-family: 'Oswald', sans-serif;
+  font-size: 1.2em;
+  line-height: 40px;
+  height: 40px;
+  border-bottom: 1px solid #888;
+}
+ 
+.nav a {
+  text-decoration: none;
+  color: #fff;
+  display: block;
+  transition: .3s background-color;
+}
+ 
+.nav a:hover {
+  background-color: #005f5f;
+}
+ 
+.nav a.active {
+  background-color: #fff;
+  color: #444;
+  cursor: default;
+}
+ 
+@media screen and (min-width: 600px) {
+  .nav li {
+    width: 120px;
+    border-bottom: none;
+    height: 50px;
+    line-height: 50px;
+    font-size: 1.4em;
+  }
+
+  .nav li {
+    display: inline-block;
+    margin-right: -4px;
+  }
+ 
+}
  #article {
-  margin:0 1em 0 16em;
-  background:#efeffe;
-  border:1px solid #01da02;
-  padding:0.2em 0.4em;
+ margin:0 1em 0 16em;
+ background:#efeffe;
+ border:1px solid #01da02;
+ padding:0.2em 0.4em;
  }
  #footer {
-  clear:both;
-  position:relative;
-  background:#edfeed;
-  border:1px solid #dada01;
-  width:99%;
-  margin:2em auto 0.5em auto;
+ clear:both;
+ position:relative;
+ background:#edfeed;
+ border:1px solid #dada01;
+ width:99%;
+ margin:2em auto 0.5em auto;
  }
  --></style>
 </head>
@@ -85,19 +112,22 @@ header('Content-type: text/html; charset=utf-8');
  <h1><?php echo $title; ?></h1>
 </header>
 
+
+<div class="nav">
+ <ul>
+ <li><a href="index.php" title="Home page">Home</a></li>
+ <li><a href="index.php?pg=about" title="About">About</a></li>
+ <li><a href="index.php?pg=courses" title="Images">courses</a></li>
+ <li><a href="index.php?pg=blogs" title="Images">blogs</a></li>
+ <li><a href="index.php?pg=contacts" title="Images">contacts</a></li>
+ </ul>
+</div>
 <section id="posts">
- <nav id="menu">
-  <ul>
-   <li><a href="index.php" title="Home page">Home</a></li>
-   <li><a href="index.php?pg=about_me" title="About Me">About Me</a></li>
-   <li><a href="index.php?pg=images" title="Images">Images</a></li>
-  </ul>
- </nav>
- <article id="article"><?php echo file_get_contents('pages/'. $pgname. '.htm'); ?></article>
+ <article id="article"><?php echo file_get_contents('pages/'. $pagename. '.html'); ?></article>
 </section>
 
 <footer id="footer">
- <p>From: <a href="http://coursesweb.net/php-mysql/" title="Free PHP-MySQL course">PHP-MySQL Course</a></p>
+ <p>From: <a href="https://coursesweb.net/php-mysql/" title="Free PHP-MySQL course">PHP-MySQL Course</a></p>
 </footer>
 
 </body>
