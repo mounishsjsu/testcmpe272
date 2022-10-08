61fa5faa9a8ef91182092bb630648a513a0287c1
mounishsjsu <111546831+mounishsjsu@users.noreply.github.com>
2022-10-07 17:26:38 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,27 +1,4 @@
-<?php
-$pagegdata = array();
-$pagedata['index'] = array(
- 'title'=>'Fitness Club',
-);
-$pagedata['about'] = array(
- 'title'=>'What is this page about',
-);
-$pagedata['courses'] = array(
- 'title'=>'Title for Images',
-);
-$pagedata['blogs'] = array(
-    'title'=>'Title for Images',
-);
-$pagedata['contacts'] = array(
-    'title'=>'Title for Images',
-);
-
-
-$pagename = isset($_GET['pg']) ? trim(strip_tags($_GET['pg'])) : 'index';
-
-$title = $pagedata[$pagename]['title'];
-
-// set header for utf-8 encode
-header('Content-type: text/html; charset=utf-8');
-?>
-<?php echo file_get_contents('pages/'. $pagename. '.html'); ?>
+<?php echo file_get_contents("html/header.html"); ?>
+<?php echo file_get_contents("html/body.html"); ?>
+<?php echo file_get_contents("html/footer.html"); ?>
+<p>Copyright © CM Cawley <?php echo date("Y"); ?></p> 
