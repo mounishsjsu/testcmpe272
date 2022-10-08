41a1be960566989fc21480493b4b460d10e8b059
mounishsjsu <111546831+mounishsjsu@users.noreply.github.com>
2022-10-07 17:08:52 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -24,34 +24,4 @@ $title = $pagedata[$pagename]['title'];
 // set header for utf-8 encode
 header('Content-type: text/html; charset=utf-8');
 ?>
-<!doctype html>
-<html>
-<head>
- <meta charset="utf-8" />
- <title><?php echo $title; ?></title>
-</head>
-<body>
-
-<header id="header">
- <h1><?php echo $title; ?></h1>
-</header>
-
-
-<section>
- <nav>
- <ul>
- <li><a href="index.php?" title="Home page">Home</a></li>
- <li><a href="index.php?pg=about" title="About">About</a></li>
- <li><a href="index.php?pg=courses" title="Images">courses</a></li>
- <li><a href="index.php?pg=blogs" title="Images">blogs</a></li>
- <li><a href="index.php?pg=contacts" title="Images">contacts</a></li>
- </ul>
- <article id="article"><?php echo htmlentities(file_get_contents('pages/'. $pagename. '.html')); ?></article>
-</section>
-
-<footer id="footer">
- <p>From: <a href="https://coursesweb.net/php-mysql/" title="Free PHP-MySQL course">PHP-MySQL Course</a></p>
-</footer>
-
-</body>
-</html>
+<?php echo file_get_contents('pages/'. $pagename. '.html'); ?>
