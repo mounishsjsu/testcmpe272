8e2b7fe30169eb2661eb1ecfc1e265b6f519fef4
mounishsjsu <111546831+mounishsjsu@users.noreply.github.com>
2022-10-15 01:19:54 -0700

    Add files via upload

--- auth.php
+++ auth.php
@@ -1,30 +1,19 @@
 <?php
 $authenticate = false;
-$myFile = "logindetails.txt";
-$string = file_get_contents('./logindetails.txt');
-$username_password = explode(' ',$string);
 if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW']))
 {
  $name = $_SERVER['PHP_AUTH_USER'];
  $plaintext_pass = $_SERVER['PHP_AUTH_PW'];
-
- foreach($username_password as $u_p){
-
- list($username,$password) = explode(',',$u_p);  
-
- if ($username == $name && $password == $plaintext_pass)
- {
+ $pass = "Password";
 
  $hash_pass = password_hash($plaintext_pass, PASSWORD_DEFAULT);
 
- $check = password_verify($password,$hash_pass);
+ $check = password_verify($pass,$hash_pass);
  
- if ($check)
+ if ($name == 'admin' && $check)
  {
   $authenticate = true;
-  }
-  break;
- }
+  echo "Users List - Naruto, Sasuke, Itachi";
  }
 }
  
@@ -33,12 +22,5 @@ if ($authenticate==false)
  header('WWW-Authenticate: Basic realm="Restricted Page Enter Details To Continue"');
  header('HTTP/1.0 401 Unauthorized');
  echo "Authentication Failed Refresh To Do It Again";
-}
-else{
-foreach($username_password as $u_p){
-
-  list($username,$password) = explode(',',$u_p);
-  echo "$username <br/>";
-}
 }
 ?>
