5c43b839b0f1dd79e5a88cd58460913bcd11cf95
mounishsjsu <111546831+mounishsjsu@users.noreply.github.com>
2022-10-15 11:16:06 -0700

    Add files via upload

--- auth.php
+++ auth.php
@@ -1,44 +1,47 @@
 <?php
 $authenticate = false;
-$myfile = fopen("logindetails.txt", "r") or die("Unable to open file!");
-$string = fread($myfile,filesize("logindetails.txt"));
-$username_password = explode(' ',$string);
 if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW']))
 {
- $name = $_SERVER['PHP_AUTH_USER'];
- $plaintext_pass = $_SERVER['PHP_AUTH_PW'];
+    $myfile = fopen("logindetails.txt", "r") or die("Unable to open file!");
+    $string = fread($myfile, filesize("logindetails.txt"));
+    $username_password = explode(' ', $string);
+    $name = $_SERVER['PHP_AUTH_USER'];
+    $plaintext_pass = $_SERVER['PHP_AUTH_PW'];
 
- foreach($username_password as $u_p){
+    foreach ($username_password as $u_p)
+    {
 
- list($username,$password) = explode(',',$u_p);  
+        list($username, $password) = explode(',', $u_p);
 
- if ($username == $name && $password == $plaintext_pass)
- {
+        if ($username == $name && $password == $plaintext_pass)
+        {
 
- $hash_pass = password_hash($plaintext_pass, PASSWORD_DEFAULT);
+           // $hash_pass = password_hash($plaintext_pass, PASSWORD_DEFAULT);
 
- $check = password_verify($password,$hash_pass);
- 
- if ($check)
- {
-  $authenticate = true;
-  }
-  break;
- }
- }
+           //  $check = password_verify($password, $hash_pass);
+
+            if ($check)
+            {
+                $authenticate = true;
+            }
+            break;
+        }
+    }
 }
- 
-if ($authenticate==false)
+
+if ($authenticate == false)
 {
- header('WWW-Authenticate: Basic realm="Restricted Page Enter Details To Continue"');
- header('HTTP/1.0 401 Unauthorized');
- echo "Authentication Failed Refresh To Do It Again";
+    header('WWW-Authenticate: Basic realm="Restricted Page Enter Details To Continue"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo "Authentication Failed Refresh To Do It Again";
 }
-else{
-foreach($username_password as $u_p){
+else
+{
+    foreach ($username_password as $u_p)
+    {
 
-  list($username,$password) = explode(',',$u_p);
-  echo "$username <br/>";
-}
+        list($username, $password) = explode(',', $u_p);
+        echo "$username <br/>";
+    }
 }
 ?>
