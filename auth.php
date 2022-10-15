66b420133bede2c555b34385bd3fa6b449e7e9db
mounishsjsu <111546831+mounishsjsu@users.noreply.github.com>
2022-10-15 00:12:55 -0700

    Add files via upload

--- auth.php
+++ auth.php
@@ -1,20 +1,40 @@
 <?php
 $authenticate = false;
+$myFile = "logindetails.txt";
+$string = file_get_contents($myFile);
 if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW']))
 {
+ $username_password = explode(' ',$string);
+
+ foreach($username_password as $u_p){
+
+ list($username,$password) = explode(',',$u_p);  
+ 
  $name = $_SERVER['PHP_AUTH_USER'];
  $plaintext_pass = $_SERVER['PHP_AUTH_PW'];
- $pass = "Password";
+
+ if($username == $name && $password == $plaintext_pass)
+ {
 
  $hash_pass = password_hash($plaintext_pass, PASSWORD_DEFAULT);
 
  $check = password_verify($pass,$hash_pass);
  
- if ($name == 'admin' && $check)
+ if ($check)
  {
   $authenticate = true;
-  echo "Users List - Naruto, Sasuke, Itachi";
  }
+ }
+ }
+ if ($authenticate==true)
+{
+ foreach($username_password as $u_p){
+
+ list($username,$password) = explode(',',$u_p);
+  echo "$username <br/>";
+ }
+}
+
 }
  
 if ($authenticate==false)
