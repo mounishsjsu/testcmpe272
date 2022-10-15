4642b8f3ee41180f790e0bf99111029f5ab244b2
mounishsjsu <111546831+mounishsjsu@users.noreply.github.com>
2022-10-15 11:28:59 -0700

    Add files via upload

--- auth.php
+++ auth.php
@@ -4,7 +4,11 @@ if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW']))
 {
     $myfile = fopen("logindetails.txt", "r") or die("Unable to open file!");
     //$string = fread($myfile, filesize("logindetails.txt"));
-    $string = 'Naruto,Naruto@1 Itachi,Itachi@1';
+    while (!feof($myfile))
+    {
+        $stringfgets($file) . "<br />";
+    }
+    //$string = 'Naruto,Naruto@1 Itachi,Itachi@1';
     $username_password = explode(' ', $string);
     $name = $_SERVER['PHP_AUTH_USER'];
     $plaintext_pass = $_SERVER['PHP_AUTH_PW'];
@@ -17,10 +21,8 @@ if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW']))
         if ($username == $name && $password == $plaintext_pass)
         {
 
-           // $hash_pass = password_hash($plaintext_pass, PASSWORD_DEFAULT);
-
-           //  $check = password_verify($password, $hash_pass);
-
+            // $hash_pass = password_hash($plaintext_pass, PASSWORD_DEFAULT);
+            //  $check = password_verify($password, $hash_pass);
             if ($check)
             {
                 $authenticate = true;
